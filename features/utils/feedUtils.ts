8cd3b77487fdba68b1f6e7e57c4ae2f67d134676
neko-shinshi <claude.hjw@gmail.com>
2023-07-24 00:47:07 +0800

    Update feedUtils.ts

--- features/utils/feedUtils.ts
+++ features/utils/feedUtils.ts
@@ -5,25 +5,29 @@ export const getMyFeeds = async (agent, db) => {
     let my = await getCustomFeeds(agent);
     const did = agent.session.did;
     const regex = new RegExp(`^at://${did}`);
-    const editableFeeds = await db.feeds.find({_id: regex}).toArray();
-    const saved = await getSavedFeeds(agent);
+    const editableFeeds = await db.feeds.find({_id: regex}).project({_id:1}).toArray();
+    let saved = await getSavedFeeds(agent);
 
     my = my.reduce((acc, x) => {
         const editable = editableFeeds.find(y => y._id === x.uri);
         const found = saved.find(y => y.uri === x.uri);
         if (found) {
             found.my = true;
+            if (editable) {
+                found.edit = true;
+            }
         } else {
             x.my = true;
+            if (editable) {
+                x.edit = true;
+            }
             acc.push(x);
         }
-        if (editable) {
-            x.edit = true;
-        }
+
         return acc;
     }, []);
 
-    return [...my, ...saved];
+    return [ ...saved, ...my];
 }
 
 export const getMyFeedIds = async (agent) => {
