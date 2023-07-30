6622ce514963e499d5d24d00faaac9cc9368d319
neko-shinshi <claude.hjw@gmail.com>
2023-07-30 09:43:58 +0800

    Update app.bsky.feed.getFeedSkeleton.tsx

--- pages/xrpc/app.bsky.feed.getFeedSkeleton.tsx
+++ pages/xrpc/app.bsky.feed.getFeedSkeleton.tsx
@@ -16,9 +16,7 @@ const getSortMethod = (sort) => {
 export async function getServerSideProps({req, res, query}) {
     try {
         res.setHeader("Content-Type", "application/json");
-    } catch (e){
-        console.log(e);
-    }
+    } catch {}
     let {feed:feedId, cursor:queryCursor, limit:_limit=50} = query;
     if (!feedId) { return { redirect: { destination: '/400', permanent: false } } }
 
