f51bae90c3fd89b3fa4d6481e5cb977542c719c5
neko-shinshi <claude.hjw@gmail.com>
2023-09-15 00:09:15 +0800

    stop dev server from updating scores

--- server.js
+++ server.js
@@ -48,11 +48,13 @@ app.prepare().then(async () => {
     server.listen(port, async (err) => {
         if (err) throw err;
         const db = await connectToDatabase();
-        await updateScores(db);
-
-        const job = Cron('*/7 * * * *', async () => {
+        if (process.env.NEXT_PUBLIC_DEV !== "1") {
             await updateScores(db);
-        });
+            const job = Cron('*/7 * * * *', async () => {
+                await updateScores(db);
+            });
+        }
+
 
         console.log(`> Ready on http${secure? "s":""}://${hostname}:${port}`);
     });
