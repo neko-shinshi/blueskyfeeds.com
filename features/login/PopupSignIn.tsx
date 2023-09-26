16b44cb09357da370ecd0f2504fb5fca375226a6
neko-shinshi <claude.hjw@gmail.com>
2023-09-27 07:02:51 +0800

    Update PopupSignIn.tsx

--- features/login/PopupSignIn.tsx
+++ features/login/PopupSignIn.tsx
@@ -2,9 +2,7 @@ import FormSignIn from "features/login/FormSignIn";
 import Popup from "features/components/Popup";
 
 export default function PopupSignIn({isOpen, setOpen}) {
-    return (
-        <Popup isOpen={isOpen} setOpen={setOpen}>
-            <FormSignIn />
-        </Popup>
-    )
+    return <Popup isOpen={isOpen} setOpen={setOpen}>
+        <FormSignIn />
+    </Popup>
 }
